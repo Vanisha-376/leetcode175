ba681c71b1193f80a31da4d4f4b0823b3d3526af
Vanisha Shankar <vanishashankar376@gmail.com>
2026-08-13 21:59:53 +0530

    File updated at 8/13/2026, 9:59:53 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,23 +1,57 @@
-// Last updated: 8/13/2026, 9:58:32 PM
-1
-2class Solution {
-3    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-4      ListNode l3 = new ListNode(0);  
-5      ListNode head = l3;
-6      int carry=0;
-7      while(l1!=null || l2!=null || carry!=0){
-8             int v1 = (l1!=null)? l1.val:0;
-9             int v2 = (l2!=null)? l2.val:0;
-10             int sum = v1+v2+carry;
-11             carry =sum/10;
-12             int digit = sum%10;
-13             l3.next =new ListNode(digit);
-14             l3=l3.next;
-15             l1=(l1!=null)?l1.next:null;
-16             l2=(l2!=null)?l2.next:null;
-17
-18      }
-19      return head.next;
-20    }
-21}
-22
+// Last updated: 8/13/2026, 9:59:53 PM
+1class Solution {
+2
+3    public void reorderList(ListNode head) {
+4
+5        if(head == null || head.next == null){
+6            return;
+7        }
+8
+9        // Step 1: Find Middle
+10
+11        ListNode slow = head;
+12        ListNode fast = head;
+13
+14        while(fast.next != null && fast.next.next != null){
+15            slow = slow.next;
+16            fast = fast.next.next;
+17        }
+18
+19        // Step 2: Split List
+20
+21        ListNode second = slow.next;
+22        slow.next = null;
+23
+24        // Step 3: Reverse Second Half
+25
+26        ListNode prev = null;
+27
+28        while(second != null){
+29
+30            ListNode next = second.next;
+31
+32            second.next = prev;
+33
+34            prev = second;
+35            second = next;
+36        }
+37
+38        second = prev;
+39
+40        // Step 4: Merge Both Halves
+41
+42        ListNode first = head;
+43
+44        while(second != null){
+45
+46            ListNode temp1 = first.next;
+47            ListNode temp2 = second.next;
+48
+49            first.next = second;
+50            second.next = temp1;
+51
+52            first = temp1;
+53            second = temp2;
+54        }
+55    }
+56}
