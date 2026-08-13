82273d9630ccb1c3b85f5d14b56d4175a5ac7fbe
Vanisha Shankar <vanishashankar376@gmail.com>
2026-08-13 21:39:40 +0530

    File updated at 8/13/2026, 9:39:40 PM

--- 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
+++ 3016-MinimumNumberofPushestoTypeWordII/3016-MinimumNumberofPushestoTypeWordII.java
@@ -1,15 +1,26 @@
-// Last updated: 8/13/2026, 9:34:11 PM
+// Last updated: 8/13/2026, 9:39:40 PM
 1class Solution {
-2    public int minimumPushes(String word) {
-3        int[] freq = new int[26];
-4        for (char c : word.toCharArray()) 
-5            freq[c - 'a']++;
-6        
-7        Arrays.sort(freq);
-8        int ans = 0;
-9        for (int i = 25; i >= 0 && freq[i] > 0; i--) 
-10            ans += freq[i] * ((25 - i) / 8 + 1);
-11        
-12        return ans;
-13    }
-14}
+2    public String countAndSay(int n) {
+3        if (n == 1) {
+4            return "1";
+5        }
+6
+7        String say = countAndSay(n - 1);
+8
+9        String result = "";
+10
+11        for (int i = 0; i < say.length(); i++) {
+12            char ch = say.charAt(i);
+13            int count = 1;
+14
+15            while (i < say.length() - 1 && say.charAt(i) == say.charAt(i + 1)) {
+16                count++;
+17                i++;
+18            }
+19
+20            result += count + "" + ch;
+21        }
+22
+23        return result;
+24    }
+25}
